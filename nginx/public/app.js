94939b05cb52041dde37a769a4e3cbda41872cac
Gary Ye <garysye@gmail.com>
2016-01-10 23:15:48 -0800

    Adds constant updating and styling to Nginx page, adds comments
    Page will now be updated every minute with new information
    Adds comments for more clarity

--- nginx/public/app.js
+++ nginx/public/app.js
@@ -2,106 +2,94 @@ $(document).ready(function() {
   var logs = {aggregate: {}, raw: [], lastUpdates: {}};
   var logsHelloWorld = {aggregate: {}, raw: []};
 
+  // Updates logs with latest data from server
   var aggregateLogs = function(newLogs, logs, lastInd) {
-    // var dateMethods = {
-    //   first: {
-    //     name: 'year',
-    //     method: 'getFullYear',
-    //     next: {
-    //       name: 'month',
-    //       method: 'getMonth',
-    //       next: {
-    //         name: 'day',
-    //         method: 'getDay',
-    //         next: {
-    //           name: 'hour',
-    //           method: 'getHours',
-    //           next: {
-    //             name: 'minute',
-    //             method: 'getMinutes',
-    //           }
-    //         }
-    //       }
-    //     }
-    //   }
-    // };
-    var log, date, ip, dateLevel, aggregateLevel, dateLevelVal, dateMinutes;
+    var log, date, ip, dateMinutes;
+    // If no last index passed, dfeault to -1
     lastInd = lastInd || - 1;
+
+    // Starting on the last index, iterates through the updates, stores the raw log, and updates the aggregate
     for (var i = lastInd + 1; i < newLogs.length; i++) {
       log = newLogs[i];
-      // date = new Date(log.timestamp);
-      dateMinutes = new Date(log.timestamp).setMinutes(0, 0, 0);
+      dateMinutes = new Date(log.timestamp).setSeconds(0, 0);
       ip = log.IP;
       logs.aggregate[ip] = logs.aggregate[ip] || {};
       logs.aggregate[ip][dateMinutes] = logs.aggregate[ip][dateMinutes] + 1 || 1;
       logs.raw.push(log);
-      // dateLevel = dateMethods.first;
-      // aggregateLevel = aggregate;
-      // dateLevelVal;
-      // while (dateLevel) {
-      //   dateLevelVal = date[dateLevel.method]();
-      //   aggregateLevel[dateLevelVal] = aggregateLevel[dateLevelVal] || {};
-      //   aggregateLevel = aggregateLevel[dateLevelVal];
-      //   dateLevel = dateLevel.next;
-      // }
-      // if (i === lastInd + 1 && aggregate[i].date === dateMinutes) {
-      //   aggregate
-      // }
     };
     return logs;
   }
 
+  // Upon update from the server, goes through the logs and updates the DOM as needed;
   var updateLogsDisplay = function(logs, className, lastUpdateInd) {
-    console.log(lastUpdateInd);
-    var $logs = $('.' + className);
-    var $raw = $logs.children('.display-raw');
-    var $aggregate = $logs.children('.display-aggregate');
+    var $logs = $("." + className);
+    var $raw = $logs.children(".display-raw");
+    var $aggregate = $logs.children(".display-aggregate");
     var $newRow, log, $ip, $dateRow, ipId, dateId;
+    // Goes from the last index, and adds new logs to the raw logs table
     for (var i = lastUpdateInd + 1, len = logs.raw.length; i < len; i++) {
       log = logs.raw[i];
-      $newRow = $('<tr>');
-      $('<div>').text('IP: ' + log.IP).appendTo($newRow);
-      $('<div>').text('Time: ' + new Date(log.timestamp).toDateString()).appendTo($newRow);
+      $newRow = $("<tr>").addClass("row-raw");
+      $("<div>").text("IP: " + log.IP).appendTo($newRow);
+      $("<div>").text("Time: " + new Date(log.timestamp).toLocaleString()).appendTo($newRow);
       $raw.append($newRow);
     }
+    // Goes through the aggregate and updates them as needed
     for (var ip in logs.aggregate) {
+      // Modifies the ip to a HTML-usable id and tries to locate the specific table
       ipId = ip.replace(/[.]/g, '-');
-      $ip = $aggregate.children('#' + ipId);
+      $ip = $aggregate.children("#" + ipId);
+      // If it cannot find an existing table for that IP, creates a new one
       if ($ip.length === 0) {
-        $ip = $('<ul>').attr('id', ipId).text('IP :' + ip);
-        $ip.appendTo('<tr>').appendTo($aggregate);
+        $ip = $("<table>").attr("id", ipId).addClass("table-aggregate");
+        $("<tr>").text("IP :" + ip).addClass("entry-ip").appendTo($ip);
+        $ip.appendTo("<tr>").appendTo($aggregate);
       }
+      // For each date with an aggregated count, it will try to find the right element; creates one if it cannot find one
       for (var date in logs.aggregate[ip]) {
-        dateId = date.replace('.', '-');
-        $dateRow = $ip.children('#' + dateId);
+        $dateRow = $ip.find("#" + date);
         if ($dateRow.length === 0) {
-          $dateRow = $('<li>').attr('id', dateId);
+          $dateRow = $("<tr>").attr("id", date).addClass("entry-date");
           $ip.append($dateRow);
         }
-        $dateRow.text(new Date(+date).toDateString() + ': ' + logs.aggregate[ip][date]);
+        $dateRow.text(new Date(+date).toLocaleString() + ": " + logs.aggregate[ip][date]);
       }
     }
   };
 
-  $.ajax({
-    url: '/v1/logs',
-    method: 'GET'
-  })
-    .done(function(data) {
-      var lastUpdateInd = logs.raw.length - 1;
-      for (var key in data.logs) {
-        aggregateLogs(data.logs[key], logs, logs.lastUpdates[key]);
-        logs.lastUpdates[key] = data.logs[key].length - 1;
-      }
-      updateLogsDisplay(logs, 'logs-all', lastUpdateInd);
-    });
-  $.ajax({
-    url: '/v1/hello-world/logs',
-    method: 'GET'
-  })
-    .done(function(data) {
-      var lastUpdateInd = logsHelloWorld.raw.length - 1;
-      aggregateLogs(data, logsHelloWorld, lastUpdateInd);
-      updateLogsDisplay(logsHelloWorld, 'logs-hello-world', lastUpdateInd);
-    });
+  // Gets all logs from the /v1/logs endpoint
+  var getAllLogs = function () {
+    $.ajax({
+      url: '/v1/logs',
+      method: 'GET'
+    })
+      .done(function(data) {
+        var lastUpdateInd = logs.raw.length - 1;
+        for (var key in data.logs) {
+          aggregateLogs(data.logs[key], logs, logs.lastUpdates[key]);
+          logs.lastUpdates[key] = data.logs[key].length - 1;
+        }
+        updateLogsDisplay(logs, 'logs-all', lastUpdateInd);
+      });
+  };
+  // Gets all logs from the /v1/hello-world/logs endpoint
+  var getHelloWorldLogs = function() {
+    $.ajax({
+      url: '/v1/hello-world/logs',
+      method: 'GET'
+    })
+      .done(function(data) {
+        var lastUpdateInd = logsHelloWorld.raw.length - 1;
+        aggregateLogs(data, logsHelloWorld, lastUpdateInd);
+        updateLogsDisplay(logsHelloWorld, 'logs-hello-world', lastUpdateInd);
+      });
+  };
+
+  // Kicks off API requests and call its again every minute
+  getAllLogs();
+  getHelloWorldLogs();
+  setInterval(function() {
+    getAllLogs();
+    getHelloWorldLogs();
+  }, 60000);
 });
--- node/api/api.js
+++ node/api/api.js
@@ -7,17 +7,17 @@ var client = redis.createClient('6379', 'redis');
 var toArray = require('stream-to-array');
 
 module.exports = function(app) {
+  // For GET at this endpoint, push it to Redis and sends a message
   app.get('/hello-world', function(req, res) {
     client.lpush(['hello-world', JSON.stringify({IP: req.ip.split(':').pop(), timestamp: Date.now()})], function(err, reply) {
       if (err) {
-        console.log('Error: ' + err);
-      } else {
-        console.log('Reply: ' + reply);
+        console.error('Error: ' + err);
       }
     });
     res.send({message: 'hello world'});
   });
 
+  // For GET at this endpoint, gets all logs stored in Redis for the endpoint and responds with it
   app.get('/hello-world/logs', function(req, res) {
     client.lrange('hello-world', 0, -1, function(err, logs) {
       if (err) {
@@ -28,14 +28,16 @@ module.exports = function(app) {
     });
   });
 
+  // For GET at this endpoint, gets all logs stored for every endpoint in Redis and responds with it
   app.get('/logs', function(req, res) {
     var data = {logs: {}, errs: {}};
     var promises = [];
+    // Gets all keys stored in Redis
     toArray(client.scan(), function(err, keys) {
       if (err) {
         res.status(404).send(err);
       } else {
-        console.log(keys);
+        // For each key, gets data stored for each key and builds it up
         keys.forEach(function(key) {
           promises.push(client.lrange(key, 0, -1)
             .catch(function(err) {
@@ -44,11 +46,10 @@ module.exports = function(app) {
             .then(function(logs) {
                 data.logs[key] = parseLogs(logs);
             }));
-          console.log(promises);
         });
+        // When data is retrieved for all keys, resonds with it
         Promise.all(promises)
           .then(function() {
-            console.log(data);
             res.send(data);
           });
       }
@@ -56,6 +57,7 @@ module.exports = function(app) {
   });
 };
 
+// Since logs are stored as stringified hashes, parse each one
 var parseLogs = function(logs) {
   logs.forEach(function(log, ind) {
     logs[ind] = JSON.parse(log);
